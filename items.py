f3acc37e7a677e760802bda1a7bea71903df91ae
Tom <tc536890"gmail.com>
2018-10-18 14:35:27 +0100

    Improving the Game Upload

---
+++ items.py
@@ -0,0 +1,72 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    'mass': 0.5,
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    'mass': 2,
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!"
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    'mass': 0.5,
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees."
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    'mass': 1,
+
+    "description": "A pack of biscuits."
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+
+    'mass': 0.5,
+
+    "description": "A basic ballpoint pen."
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+
+    'mass': 1,
+
+    "description": "This student handbook explains everything. Seriously."
+}
+
+items = {
+    'id' : item_id,
+    'laptop' : item_laptop,
+    'money' : item_money,
+    'biscuits' : item_biscuits,
+    'pen' : item_pen,
+    'handbook' : item_handbook
+}
